d065bc88462a0ef6f171a771ae83ab0d24c7ca4b
Tsitsip2151n <141562921+Rabenarison-Andry-Tsiresy@users.noreply.github.com>
2026-05-07 14:17:49 +0300

    update

---
+++ app/Database/Migrations/2026-04-20-000001_CreateLivresTable.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateLivresTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'titre' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'auteur' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'isbn' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'annee_publication' => [
+                'type'       => 'SMALLINT',
+                'constraint' => 4,
+            ],
+            'categorie' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'resume' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'couverture' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => true,
+            ],
+            'statut' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
+                'default'    => 'disponible',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addUniqueKey('isbn');
+        $this->forge->createTable('livres');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('livres');
+    }
+}
